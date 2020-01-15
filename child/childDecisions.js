78998d4872338fd31d0e91be64b2e5dd5efe2c32
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2020-01-15 14:37:51 +0530

    #20 | Child PNC cluster Incharge Rules

--- child/childDecisions.js
+++ child/childDecisions.js
@@ -5,6 +5,8 @@ import {RuleFactory ,
 
 
 const followupDecision = RuleFactory("4548364c-ff22-447b-baec-3c63935a7e00", "Decision");
+const followupDecisionClusterIncharge =  RuleFactory("80f09382-c97b-4850-9c9e-b834e0a6f501", "Decision");
+
 
 const zScoreGradeStatusMappingWeightForAge = {
     '1': 'Normal',
@@ -87,55 +89,65 @@ const addIfRequired = (decisions, name, value) => {
 @followupDecision("39021909-fad8-49c1-9892-2c266b08a752", "Follow Up Decision", 100)
 export class FollowDecisions {
     static exec(programEncounter, decisions, context, today) {
+        followupDecisions(programEncounter, decisions, context, today);
+    }    
+}
 
-        const weight = programEncounter.getObservationValue("Current Weight");
-        const height = programEncounter.getObservationValue("Height");
-        const encounterDateTime = programEncounter.encounterDateTime;
-        const individual = programEncounter.programEnrolment.individual;
+@followupDecisionClusterIncharge("0b3a820e-369f-4a7a-a54b-bc44ef13a31e", "Follow Up Decision Cluster Incharge", 100)
+export class FollowDecisionsClusterIncharge {
+    static exec(programEncounter, decisions, context, today) {
+        followupDecisions(programEncounter, decisions, context, today);
+    }    
+}
 
-        const nutritionalStatus = nutritionalStatusForChild(individual, encounterDateTime, weight, height);
+const followupDecisions = (programEncounter, decisions, context, today) => {
+    const weight = programEncounter.getObservationValue("Current Weight");
+    const height = programEncounter.getObservationValue("Height");
+    const encounterDateTime = programEncounter.encounterDateTime;
+    const individual = programEncounter.programEnrolment.individual;
 
-        console.log('nutritionalStatus decisions',nutritionalStatus);
+    const nutritionalStatus = nutritionalStatusForChild(individual, encounterDateTime, weight, height);
 
-        addIfRequired(decisions.encounterDecisions, "Weight for age z-score", nutritionalStatus.wfa);
-        addIfRequired(decisions.encounterDecisions, "Weight for age Grade", nutritionalStatus.wfaGrade);
-        addIfRequired(decisions.encounterDecisions, "Weight for age Status", nutritionalStatus.wfaStatus ? [nutritionalStatus.wfaStatus] : []);
-               
-        addIfRequired(decisions.encounterDecisions, "Weight for height z-score", nutritionalStatus.wfh);
-        // addIfRequired(decisions.encounterDecisions, "Weight for Height Status", nutritionalStatus.wfhStatus ? [nutritionalStatus.wfhStatus] : []);
-        
-        // console.log('decisions',decisions);
-        decisions.encounterDecisions.push(FollowDecisions.referToHospital(programEncounter));
-        // console.log('decisions',decisions);
-        return decisions;
-    }
+    console.log('nutritionalStatus decisions',nutritionalStatus);
 
-    static referToHospital(programEncounter) {
-        const decisionBuilder = new ComplicationsBuilder({
-            programEncounter: programEncounter,
-            complicationsConcept: "Refer to the hospital for"
-        });
+    addIfRequired(decisions.encounterDecisions, "Weight for age z-score", nutritionalStatus.wfa);
+    addIfRequired(decisions.encounterDecisions, "Weight for age Grade", nutritionalStatus.wfaGrade);
+    addIfRequired(decisions.encounterDecisions, "Weight for age Status", nutritionalStatus.wfaStatus ? [nutritionalStatus.wfaStatus] : []);
+           
+    addIfRequired(decisions.encounterDecisions, "Weight for height z-score", nutritionalStatus.wfh);
+    // addIfRequired(decisions.encounterDecisions, "Weight for Height Status", nutritionalStatus.wfhStatus ? [nutritionalStatus.wfhStatus] : []);
+    
+    // console.log('decisions',decisions);
+    decisions.encounterDecisions.push(referToHospital(programEncounter));
+    // console.log('decisions',decisions);
+    return decisions;
+}
 
-        decisionBuilder.addComplication("Not able to drink or breastfeed")
-            .when.valueInEncounter("Is the child able to drink or breastfeed").is.no;
-        decisionBuilder.addComplication("Child Vomiting everything")
-            .when.valueInEncounter("Does the child vomits everything").is.yes;
-        decisionBuilder.addComplication("child had convulsion")
-            .when.valueInEncounter("Has the child had convulsion").is.yes;
-        decisionBuilder.addComplication("child is lethargic or unconsious")
-            .when.valueInEncounter("See the child is lethargic or unconsious").is.yes;
-        decisionBuilder.addComplication("there is general danger sign")
-            .when.valueInEncounter("Is there general danger sign").is.yes;
-        decisionBuilder.addComplication("Fast breathing")
-            .when.valueInEncounter("Does the child has fast breathing").is.yes;
-        decisionBuilder.addComplication("chest indrwaning")
-            .when.valueInEncounter("look for chest indrwaning").is.yes;
-        decisionBuilder.addComplication("child has complaint")
-            .when.valueInEncounter("does child has any other complaint").is.yes;
+const referToHospital = (programEncounter) => {
+    const decisionBuilder = new ComplicationsBuilder({
+        programEncounter: programEncounter,
+        complicationsConcept: "Refer to the hospital for"
+    });
 
-        return decisionBuilder.getComplications();
+    decisionBuilder.addComplication("Not able to drink or breastfeed")
+        .when.valueInEncounter("Is the child able to drink or breastfeed").is.no;
+    decisionBuilder.addComplication("Child Vomiting everything")
+        .when.valueInEncounter("Does the child vomits everything").is.yes;
+    decisionBuilder.addComplication("child had convulsion")
+        .when.valueInEncounter("Has the child had convulsion").is.yes;
+    decisionBuilder.addComplication("child is lethargic or unconsious")
+        .when.valueInEncounter("See the child is lethargic or unconsious").is.yes;
+    decisionBuilder.addComplication("there is general danger sign")
+        .when.valueInEncounter("Is there general danger sign").is.yes;
+    decisionBuilder.addComplication("Fast breathing")
+        .when.valueInEncounter("Does the child has fast breathing").is.yes;
+    decisionBuilder.addComplication("chest indrwaning")
+        .when.valueInEncounter("look for chest indrwaning").is.yes;
+    decisionBuilder.addComplication("child has complaint")
+        .when.valueInEncounter("does child has any other complaint").is.yes;
 
-    }
+    return decisionBuilder.getComplications();
+
 }
 
 
