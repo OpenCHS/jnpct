6b54f3548cecef233891d24ca97c2a2f36b26404
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2020-06-24 18:07:27 +0530

    User Training Feedback ++

--- metadata/rules/viewFilters.js
+++ metadata/rules/viewFilters.js
@@ -27,6 +27,36 @@ class ProgramExitViewFilterHandler {
         return statusBuilder.build();
     }
 
+   placeOfDeath(programExit, formElement) {
+    const statusBuilder = this._getStatusBuilder(programExit, formElement);
+    statusBuilder.show().when.valueInExit("Reason for exit").containsAnyAnswerConceptName("Maternal Death","Child Death");
+    return statusBuilder.build();
+   }
+
+   dateOfDeath(programExit, formElement) {
+    const statusBuilder = this._getStatusBuilder(programExit, formElement);
+    statusBuilder.show().when.valueInExit("Reason for exit").containsAnyAnswerConceptName("Maternal Death","Child Death");
+    return statusBuilder.build();
+   }
+
+   reasonOfDeath(programExit, formElement) {
+    const statusBuilder = this._getStatusBuilder(programExit, formElement);
+    statusBuilder.show().when.valueInExit("Reason for exit").containsAnyAnswerConceptName("Maternal Death","Child Death");
+    
+    const programName = programExit.program.name;
+
+        if(programName === 'Child')
+        statusBuilder.skipAnswers("APH","PPH","Sepsis","Unsafe abortion","Obstructed labour",
+                        "Prolong labour","Eclampsia","Delay");
+        else if(programName === 'Pregnancy' || programName === 'Eligible couple')
+        statusBuilder.skipAnswers("Low birth weight","Birth","Birth injury","Asphyxia",
+                        "Preterm","Sepsis","Diarrhea","Pnuemonia","Malnutrition",
+                        "Congenital abnormalities","Socialy neglected","Malaria");
+
+    return statusBuilder.build();
+   }
+
+
    otherReasonPleaseSpecify(programExit, formElement) {
         const statusBuilder = this._getStatusBuilder(programExit, formElement);
         statusBuilder.show().when.valueInExit("Reason for exit").containsAnswerConceptName("Other");
@@ -67,6 +97,45 @@ class ProgramCancellationFormFilters {
         const cancelReasonObs = programEncounter.findCancelEncounterObservation('Visit cancel reason');
         const answer = cancelReasonObs && cancelReasonObs.getReadableValue();
         return new FormElementStatus(formElement.uuid, answer === 'Other');
+    }
+
+    @WithName('Place of death')
+    @statusBuilder
+    a12([programEncounter], statusBuilder) {    
+        const cancelReasonObs = programEncounter.findCancelEncounterObservation('Visit cancel reason');
+        const answer = cancelReasonObs && cancelReasonObs.getReadableValue();
+           
+        statusBuilder.show().whenItem(answer ==="Maternal Death" || answer ==="Child Death").is.truthy;
+    }
+
+    @WithName('Date of death')
+    @statusBuilder
+    a13([programEncounter], statusBuilder) {        
+        const cancelReasonObs = programEncounter.findCancelEncounterObservation('Visit cancel reason');
+        const answer = cancelReasonObs && cancelReasonObs.getReadableValue();
+           
+        statusBuilder.show().whenItem(answer ==="Maternal Death" || answer ==="Child Death").is.truthy;
+    }
+
+    @WithName('Reason of death')
+    @statusBuilder
+    a14([programEncounter], statusBuilder) {        
+        const cancelReasonObs = programEncounter.findCancelEncounterObservation('Visit cancel reason');
+        const answer = cancelReasonObs && cancelReasonObs.getReadableValue();
+           
+        statusBuilder.show().whenItem(answer ==="Maternal Death" || answer ==="Child Death").is.truthy;
+  
+        const programName = programExit.program.name;
+
+        if(programName === 'Child')
+        statusBuilder.skipAnswers("APH","PPH","Sepsis","Unsafe abortion","Obstructed labour",
+                        "Prolong labour","Eclampsia","Delay");
+        else if(programName === 'Pregnancy' || programName === 'Eligible couple')
+        statusBuilder.skipAnswers("Low birth weight","Birth","Birth injury","Asphyxia",
+                        "Preterm","Sepsis","Diarrhea","Pnuemonia","Malnutrition",
+                        "Congenital abnormalities","Socialy neglected","Malaria");
+
+   
     }
 
     static exec(programEncounter, formElementGroup, today) {
